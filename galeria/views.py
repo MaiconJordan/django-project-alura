8aa430379ea0e2a920137df1aea80f674b35b4cc
Maicon <maiconplus@gmail.com>
2025-08-05 15:01:30 -0300

    index atualizado

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the galeria index.")
+    return render(request, 'galeria/index.html')
