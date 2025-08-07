6ff287a435e2a421668af602b9ebaa6821494f85
Maicon <maiconplus@gmail.com>
2025-08-07 20:05:53 -0300

    Novo filtro de seleção, publicada

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,10 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda",  "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome", )
     list_filter = ("categoria",)
+    list_editable = ("publicada",)
 
 admin.site.register(Fotografia, ListandoFotografias)
