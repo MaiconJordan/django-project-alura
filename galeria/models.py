2a584daf3646d984fecf34bb529433a5dfc6b874
Maicon <maiconplus@gmail.com>
2025-08-08 20:08:09 -0300

    foto dinamica ao selecionar no admin

--- galeria/models.py
+++ galeria/models.py
@@ -13,7 +13,7 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, null=False, default='')
-    foto = models.CharField(max_length=150, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos', null=False, blank=False)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
